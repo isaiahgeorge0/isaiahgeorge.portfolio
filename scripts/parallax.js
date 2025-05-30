0f508886d7d2f92a97c43b9289bdd8063c0ac809
isaiahgeorge0 <isaiah120303@gmail.com>
2025-05-30 02:33:01 -0700

    .

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -1,16 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
-  if (typeof Rellax !== 'undefined') {
-    new Rellax('.rellax', {
-      speed: -2,
-      center: false,
-      wrapper: null,
-      round: true,
-      vertical: true,
-      horizontal: false
-    });
-  } else {
-    console.warn('Rellax library not loaded.');
-  }
+  const rellax = new Rellax('.rellax', {
+    speed: -2,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false,
+  });
 });
 
 
